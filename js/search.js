538f4736ab6a5c91f74012bb6d7a9a4016caae7f
ivan-kuzmin <Ivan@MacBook-Ivan.local>
2018-03-04 10:14:39 +0300

    add new transliterate

--- js/search.js
+++ js/search.js
@@ -9,22 +9,22 @@ var client = new $.es.Client({
     hosts: 'https://search-cerebro-xg6oa5f4gmcispwqp3kn3p6mta.us-west-2.es.amazonaws.com'
 });
 
-// var transliterate = (
-//     function() {
-//         var
-//         rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
-//         eng = "shh sh ch cz yu ya yo zh `` y e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g)
-//         ;
-//         return function(text, engToRus) {
-//             var x;
-//             for(x = 0; x < rus.length; x++) {
-//                 text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
-//                 text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
-//             }
-//             return text;
-//         }
-//     }
-// )();
+var transliterate = (
+    function() {
+        var
+        rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
+        eng = "shh sh ch cz yu ya yo zh `` y e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g)
+        ;
+        return function(text, engToRus) {
+            var x;
+            for(x = 0; x < rus.length; x++) {
+                text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
+                text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
+            }
+            return text;
+        }
+    }
+)();
 
 var input = document.body.children[0];
 input.oninput = function() {
@@ -35,7 +35,7 @@ input.oninput = function() {
 };
 
 function search(name) {
-    // name = "*"+transliterate(name)+"* OR *"+transliterate(transliterate(name), true)+"*"
+    name = "*"+transliterate(name)+"* OR *"+transliterate(transliterate(name), true)+"*"
     client.search({
         index: 'skoltech',
         type: 'professors',
@@ -43,7 +43,7 @@ function search(name) {
             query: {
                 query_string: {
                     fields: ["name^4","lastname^4","center^3","position^2","bio"],
-                    default_operator: "OR",
+                    // default_operator: "OR",
                     query: name
                 }
             }
